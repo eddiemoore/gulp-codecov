5efd22def6912f012134dfa39be67e3c37755c22
Ed Moore <eddie.moore@gmail.com>
2015-05-19 23:49:41 +0800

    Fixing tests

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 'use strict'
 var through = require('through2')
-var path = require('path')
 var gutil = require('gulp-util')
 var codecov = require('codecov.io')
 
 var PLUGIN_NAME = 'gulp-codecov.io'
 
-function Codecov (options) {
-  options = options || {}
+module.exports = function () {
+  return through.obj(function (file, enc, callback) {
 
-  return through.obj(function (file, enc, cb) {
-    if (file.isStream()) {
-      return cb(new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'))
-    }
-    if (file.isNull()) {
-      return cb(null, file)
-    }
-    if (path.extname(file.path) !== '.lcov' && path.extname(file.path) !== '.json') {
-      return cb(null, file)
-    }
-
-    codecov.handleInput(file.contents, function (err) {
-      if (err) {
-        console.log('error sending to codecov.io: ', err, err.stack)
-        if (/non-success response/.test(err.message)) {
-          console.log('detail: ', err.detail)
-        } else {
-          console.log('SUCCESS')
+    function sendToCodecov (input, done) {
+      codecov.handleInput(input, function (err) {
+        if (err) {
+          console.log('error sending to codecov.io: ', err, err.stack)
+          if (/non-success response/.test(err.message)) {
+            console.log('detail: ', err.detail)
+          } else {
+            console.log('SUCCESS')
+          }
+          return done(new gutil.PluginError(PLUGIN_NAME, err))
         }
-        return cb(new gutil.PluginError(PLUGIN_NAME, err))
-      }
-      return cb(null, file)
-    })
+        return done(null, file)
+      })
+    }
+
+    if (file.isNull()) {
+      this.push(file)
+      return callback()
+    }
+
+    if (file.isStream()) {
+      this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Stream content is not supported'))
+      return callback()
+    }
+
+    if (file.isBuffer()) {
+      sendToCodecov(file.contents.toString(), callback)
+      this.push(file)
+    }
   })
 }
-
-module.exports = Codecov
--- test/main.js
+++ test/main.js
@@ -3,21 +3,27 @@
 var expect = require('chai').expect
 var fs = require('fs')
 var gutil = require('gulp-util')
-var codecov = require('../')
-
-var normalContents = fs.readFileSync('test/fixtures/lcov.info')
+var gulpCodecov = require('../')
 
 describe('gulp-codecov.io', function () {
   describe('codecov()', function () {
-    it('should send files to codecov', function (done) {
-      var stream = codecov()
+    process.env.TRAVIS = 'true'
+    process.env.TRAVIS_JOB_ID = '1234'
+    process.env.TRAVIS_COMMIT = '5678'
+    process.env.TRAVIS_JOB_NUMBER = '91011'
+    process.env.TRAVIS_BRANCH = 'master'
+    process.env.TRAVIS_PULL_REQUEST = 'blah'
+    process.env.TRAVIS_REPO_SLUG = 'owner/repo'
 
+    it('should pass the file through via buffer', function (done) {
       var fakeFile = new gutil.File({
         base: 'test/fixtures',
         cwd: 'test/',
         path: 'test/fixtures/lcov.info',
-        contents: normalContents
+        contents: fs.readFileSync('test/fixtures/lcov.info')
       })
+
+      var stream = gulpCodecov()
 
       stream.on('error', function (err) {
         expect(err).to.exist
